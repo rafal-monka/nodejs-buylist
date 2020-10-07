db57a2becabb38118261465a0a301e6e1f65637a
Rafal Monka <monka.rafal@gmail.com>
2020-10-07 16:49:50 +0200

    React app in public folder (after local build) [2]

---
+++ public/precache-manifest.326b0d0d6f89e0d530b95abb20d0665d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e55ee523d425fef112ce2d30508029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a8ac274631f4a44a10",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6ad6d999a49937d7ecfe",
+    "url": "/static/css/main.be46dee8.chunk.css"
+  },
+  {
+    "revision": "59a8ac274631f4a44a10",
+    "url": "/static/js/2.3d17e397.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3d17e397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad6d999a49937d7ecfe",
+    "url": "/static/js/main.63487a7d.chunk.js"
+  },
+  {
+    "revision": "7b91a5e25d6a4b6d65ef",
+    "url": "/static/js/runtime-main.67466108.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
